3876cc5d6c6cc1cc83cc1b8d94ecc9a2024cfe5d
Shaila Pawar <shaila.pawar6@gmail.com>
2015-12-07 14:02:19 +0530

    Code cleanup and support for Edit prospect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,46 @@
 'use strict';
 
-var module = angular.module('PreSales-Huddle', ['ngResource',
-    'ngRoute'])
+angular.module('PreSales-Huddle', ['ngResource', 'ngRoute'])
+    .config(function ($routeProvider, $httpProvider) {
+        $routeProvider.when('/prospects', {
+        templateUrl: '../views/viewProspects.html',
+        controller: 'ProspectsCtrl',
+        title: 'All Prospects'
+    })
+    .when('/edit', {
+        templateUrl: '../views/editProspect.html',
+        controller: 'EditProspectCtrl',
+        title: 'Edit Prospect'
+    })
+    .when('/discussions', {
+        templateUrl: '../views/viewDiscussions.html',
+        controller: 'DiscussionsCtrl',
+        title: 'Discussions'
+    })
+    .when('/addDiscussion', {
+        templateUrl: '../views/addDiscussion.html',
+        controller: 'AddDiscussionCtrl',
+        title: 'Add Discussion'
+    })
+    .when('/create', {
+        templateUrl: '../views/addProspect.html',
+        controller: 'AddProspectCtrl',
+        title: 'Add Prospect'
+    });
 
-.config(function ($routeProvider) {
-  $routeProvider.when('/prospects', {
-    templateUrl: '../views/viewProspects.html',
-    controller: 'ProspectsCtrl',
-    title: 'All Prospects'
-  })
-  .when('/create', {
-    templateUrl: '../views/addProspect.html',
-    controller: 'AddProspectCtrl',
-    title: 'Add Prospect'
-  })
-})
+//$httpProvider.interceptors.push('httpInterceptor');
+
+
+});
+
+//module.factory('httpInterceptor', function(){
+//  return {
+//    request : function (config) {
+//        console.log(config);
+//        return config;
+//    },
+//    response : function (response) {
+//        return response;
+//    }
+//  }
+//});
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,54 +1,153 @@
 // var baseURL = "http://172.24.212.123:8280/";
-var baseURL = "http://presaleshuddle:8280/";
+var baseURL = "http://presaleshuddle:8080/";
 
 angular.module('PreSales-Huddle')
 
-  .controller('ProspectsCtrl', function($scope, $http) {
-    $http.get(baseURL + 'prospect/view/')
-      .success(function(data, status, headers, config) {
-        $scope.prospects = data;
-    }).error(function(data, status, header, config) {
-    });
-  })
 
+    .controller('ProspectsCtrl', function($scope, $http, $rootScope) {
+        // default sorting order is by Prospect Creation Date
+        $scope.orderByField = 'StartDate';
 
-  .controller('AddProspectCtrl', function($scope, $http, $rootScope,
-      $location) {
-    $scope.addProspect = function() {
+        $scope.saveData = function(prospect) {
+            //console.log("save data.");
+            console.log(prospect);
+            $rootScope.prospectToUpdate = prospect;
+        };
 
-      var date = $scope.date.toDateString();
-      console.log(date,typeof(date));
+        $http.get(baseURL + 'prospect/all/').success(function(data, status, headers, config) {
+            $scope.prospects = data;
+        }).error(function(data, status, header, config) {
+        });
+    })
 
-      var data = {
-        Name: $scope.prospectName,
-        StartDate : date,
-        TechStack: $scope.techStack,
-        Domain: $scope.domain,
-        DesiredTeamSize: $scope.teamSize,
-        Notes: $scope.notes
-      };
+    .controller('AddProspectCtrl', function($scope, $http, $rootScope, $location) {
+        $scope.maxDate = new Date();
+        $scope.date = $scope.maxDate;
+        console.log($scope.maxDate);
 
-      console.log(data);
+        $scope.addProspect = function() {
 
-      $http.post(baseURL + 'prospect/add/', data = data)
-        .success(function(data, status, headers, config) {
-          console.log('Prospect added.');
-          $location.path('/prospects');
-        }).error(function(data, status, headers, config) {
-          console.log('Prospect Not added.');
-      });
+            // var mydate = $scope.date.toString();
+            // var date = mydate.split('T')[0];
+            // console.log(date,typeof(date));
+            console.log($scope.date);
+            var data = {
+                Name: $scope.prospectName,
+                StartDate : $scope.date,
+                TechStack: $scope.techStack,
+                Domain: $scope.domain,
+                DesiredTeamSize: $scope.teamSize,
+                Notes: $scope.notes
+            };
 
-      $scope.prospectName = "";
-      $scope.date = "";
-      $scope.techStack = "";
-      $scope.domain = "";
-      $scope.teamSize = "";
-      $scope.notes = "";
-    }
+            console.log(data);
 
-    // Cancel button function
-    $scope.go = function(path) {
-      $rootScope.lastform = "create";
-      $location.path(path);
-    }
-  })
+            $http.post(baseURL + 'prospect/', data = data).success(function(data, status, headers, config) {
+                console.log('Prospect added.');
+                $location.path('/prospects');
+            }).error(function(data, status, headers, config) {
+                console.log('Prospect Not added.');
+            });
+
+            $scope.prospectName = "";
+            $scope.date = "";
+            $scope.techStack = "";
+            $scope.domain = "";
+            $scope.teamSize = "";
+            $scope.notes = "";
+        };
+
+        // Cancel button function
+        $scope.go = function(path) {
+            $rootScope.lastform = "create";
+            $location.path(path);
+        }
+    })
+
+    .controller('EditProspectCtrl', function($scope, $http, $rootScope, $location) {
+
+        var prospectFetched = $rootScope.prospectToUpdate;
+
+        console.log("date:", prospectFetched.StartDate);
+        var mydate = prospectFetched.StartDate.toString();
+        var date = mydate.split('T')[0];
+        console.log(date,typeof(date));
+        // var mydate = new Date(prospectFetched.StartDate);
+        // console.log(mydate);
+        $scope.prospect = prospectFetched;
+        $scope.prospect.StartDate = new Date(date);
+        console.log($scope.prospect.StartDate);
+
+        $scope.editProspect = function() {
+
+            var data = {
+                ProspectID: $rootScope.prospectToUpdate.ProspectID,
+                Name: $scope.prospect.Name,
+                StartDate : $scope.prospect.StartDate,
+                TechStack: $scope.prospect.TechStack,
+                Domain: $scope.prospect.Domain,
+                DesiredTeamSize: $scope.prospect.DesiredTeamSize,
+                Notes: $scope.prospect.Notes
+            };
+            console.log(data);
+
+            $http.put(baseURL + 'prospect/', data = data).success(function(data, status, headers, config) {
+                console.log('Prospect added.');
+                $location.path('/prospects');
+            }).error(function(data, status, headers, config) {
+                console.log(data, status, headers, config);
+                console.log('Prospect Not added.');
+            });
+        };
+
+        // Cancel button function
+        $scope.go = function(path) {
+            $rootScope.lastform = "create";
+            $location.path(path);
+        }
+    })
+
+    .controller('AddDiscussionCtrl', function($scope, $http, $rootScope, $location) {
+        $scope.maxDate = new Date();
+        $scope.date = $scope.maxDate;
+        console.log($scope.maxDate);
+
+        $scope.addDiscussion = function() {
+
+            // var mydate = $scope.date.toString();
+            // var date = mydate.split('T')[0];
+            // console.log(date,typeof(date));
+            console.log($scope.date);
+            var data = {
+                Name: $scope.prospectName,
+                StartDate : $scope.date,
+                TechStack: $scope.techStack,
+                Domain: $scope.domain,
+                DesiredTeamSize: $scope.teamSize,
+                Notes: $scope.notes
+            };
+
+            console.log(data);
+
+            $http.post(baseURL + 'discussion/', data = data).success(function(data, status, headers, config) {
+                console.log('Discussion added.');
+                $location.path('/prospects');
+            }).error(function(data, status, headers, config) {
+                console.log('Discussion Not added.');
+            });
+
+            $scope.prospectName = "";
+            $scope.date = "";
+            $scope.techStack = "";
+            $scope.domain = "";
+            $scope.teamSize = "";
+            $scope.notes = "";
+        };
+
+        // Cancel button function
+        $scope.go = function(path) {
+            $rootScope.lastform = "create";
+            $location.path(path);
+        }
+    })
+
