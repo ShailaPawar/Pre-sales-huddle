7ba2b6f83beda37bbdcf382813a1d0639d3d27bc
Shaila Pawar <shaila.pawar6@gmail.com>
2015-12-11 18:45:58 +0530

    - Add/View Discussions - Code refactoring

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,44 +3,54 @@
 angular.module('PreSales-Huddle', ['ngResource', 'ngRoute'])
     .config(function ($routeProvider, $httpProvider) {
         $routeProvider.when('/prospects', {
-        templateUrl: '../views/viewProspects.html',
-        controller: 'ProspectsCtrl',
-        title: 'All Prospects'
-    })
-    .when('/edit', {
-        templateUrl: '../views/editProspect.html',
-        controller: 'EditProspectCtrl',
-        title: 'Edit Prospect'
-    })
-    .when('/discussions', {
-        templateUrl: '../views/viewDiscussions.html',
-        controller: 'DiscussionsCtrl',
-        title: 'Discussions'
-    })
-    .when('/addDiscussion', {
-        templateUrl: '../views/addDiscussion.html',
-        controller: 'AddDiscussionCtrl',
-        title: 'Add Discussion'
-    })
-    .when('/create', {
-        templateUrl: '../views/addProspect.html',
-        controller: 'AddProspectCtrl',
-        title: 'Add Prospect'
-    });
+                templateUrl: '../views/viewProspects.html',
+                controller: 'ProspectsCtrl',
+                title: 'All Prospects'
+            })
+            .when('/editProspect', {
+                templateUrl: '../views/editProspect.html',
+                controller: 'EditProspectCtrl',
+                title: 'Edit Prospect'
+            })
+            .when('/createProspect', {
+                templateUrl: '../views/addProspect.html',
+                controller: 'AddProspectCtrl',
+                title: 'Add Prospect'
+            })
+            .when('/discussions', {
+                templateUrl: '../views/viewDiscussions.html',
+                controller: 'DiscussionsCtrl',
+                title: 'Discussions'
+            })
+            .when('/addDiscussion', {
+                templateUrl: '../views/addDiscussion.html',
+                controller: 'AddDiscussionCtrl',
+                title: 'Add Discussion'
+            })
+    }).run(['$window', function ($window) {
+        $window.GoogleAsyncInit = function () {
+            // Executed when the SDK is loaded
+            gapi.load('auth2', function () {
+                gapi.auth2.init();
+            });
+        };
 
-//$httpProvider.interceptors.push('httpInterceptor');
+        //Load Google SDK asynchronously
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://apis.google.com/js/platform.js?onload=GoogleAsyncInit';
+            //js.attributes ='defer async';
+            js.setAttribute('defer', '');
+            js.setAttribute('async', '');
+            fjs.parentNode.insertBefore(js, fjs);
 
-
-});
-
-//module.factory('httpInterceptor', function(){
-//  return {
-//    request : function (config) {
-//        console.log(config);
-//        return config;
-//    },
-//    response : function (response) {
-//        return response;
-//    }
-//  }
-//});
+            //Add meta Tag with client ID
+            var meta = document.createElement('meta');
+            meta.name = 'google-signin-client_id';
+            meta.content = "726910394043-ptmnorekimlpkmruvlbq1neqvpoenk6k.apps.googleusercontent.com";
+            d.getElementsByTagName('head')[0].appendChild(meta);
+        }(document, 'script', 'google-jssdk'));
+    }]);
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -3,15 +3,72 @@ var baseURL = "http://presaleshuddle:8080/";
 
 angular.module('PreSales-Huddle')
 
+    .controller('GoogleSignInCtrl', function($scope, $rootScope, $location) {
+        console.log("In Sign In Ctrl.");
+        function googleLogin(){
+            var auth2 = gapi.auth2.getAuthInstance();
+            return auth2.signIn();
+        }
+
+        $scope.onSignIn = function() {
+            googleLogin()
+                .then(function (data) {
+                    //console.log("data", data);
+                    var profile = data.getBasicProfile();
+                    //console.log(profile);
+                    $rootScope.currentUser = profile.getEmail();
+                    $rootScope.currentUserImage = profile.getImageUrl();
+                    //console.log($rootScope.currentUser);
+                    window.location = '#/prospects';
+                    document.getElementById('signin').style.visibility='hidden';
+                    document.getElementById('g-signinP').style.height = '0px';
+                    document.getElementById('sign-out').style.visibility='visible';
+                    document.getElementById('home').style.visibility='visible';
+                }, function (err) {
+                    console.log(err)
+                });
+        };
+    })
+
+    .controller('SignOutCtrl', function($scope, $rootScope, $location) {
+        console.log("In Sign Out Controller.");
+        function LogOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            return auth2.signOut();
+        }
+
+        $scope.signOut = function() {
+            LogOut()
+                .then(function() {
+                        console.log('User signed out.');
+                        window.location = '#';
+                        window.location.reload();
+                        document.getElementById('signin').style.visibility = 'visible';
+                        document.getElementById('g-signinP').style.removeProperty('height');
+                        document.getElementById('sign-out').style.visibility = 'hidden';
+                        document.getElementById('home').style.visibility = 'hidden';
+                });
+        };
+    })
 
     .controller('ProspectsCtrl', function($scope, $http, $rootScope) {
+
+            document.getElementById('signin').style.visibility='hidden';
+            document.getElementById('g-signinP').style.height = '0px';
+            document.getElementById('sign-out').style.visibility='visible';
+            document.getElementById('home').style.visibility='visible';
         // default sorting order is by Prospect Creation Date
-        $scope.orderByField = 'StartDate';
+        $scope.orderByField = 'CreateDate';
 
         $scope.saveData = function(prospect) {
-            //console.log("save data.");
-            console.log(prospect);
             $rootScope.prospectToUpdate = prospect;
+
+            $rootScope.createDate = $rootScope.prospectToUpdate.CreateDate.toString();
+            $rootScope.createDate = $rootScope.createDate.split('T')[0];
+            $rootScope.createDate = new Date($rootScope.createDate);
+
+            $rootScope.prospectToUpdate.CreateDate = $rootScope.createDate;
+
         };
 
         $http.get(baseURL + 'prospect/all/').success(function(data, status, headers, config) {
@@ -21,6 +78,11 @@ angular.module('PreSales-Huddle')
     })
 
     .controller('AddProspectCtrl', function($scope, $http, $rootScope, $location) {
+        document.getElementById('signin').style.visibility='hidden';
+        document.getElementById('g-signinP').style.height = '0px';
+        document.getElementById('sign-out').style.visibility='visible';
+        document.getElementById('home').style.visibility='visible';
+
         $scope.maxDate = new Date();
         $scope.date = $scope.maxDate;
         console.log($scope.maxDate);
@@ -30,10 +92,10 @@ angular.module('PreSales-Huddle')
             // var mydate = $scope.date.toString();
             // var date = mydate.split('T')[0];
             // console.log(date,typeof(date));
-            console.log($scope.date);
+            console.log("In AddProspectCtrl, Creation Date: ", $scope.date);
             var data = {
                 Name: $scope.prospectName,
-                StartDate : $scope.date,
+                CreateDate : $scope.date,
                 TechStack: $scope.techStack,
                 Domain: $scope.domain,
                 DesiredTeamSize: $scope.teamSize,
@@ -44,9 +106,10 @@ angular.module('PreSales-Huddle')
 
             $http.post(baseURL + 'prospect/', data = data).success(function(data, status, headers, config) {
                 console.log('Prospect added.');
+
                 $location.path('/prospects');
             }).error(function(data, status, headers, config) {
-                console.log('Prospect Not added.');
+                console.log('Prospect not added.');
             });
 
             $scope.prospectName = "";
@@ -65,25 +128,19 @@ angular.module('PreSales-Huddle')
     })
 
     .controller('EditProspectCtrl', function($scope, $http, $rootScope, $location) {
+        document.getElementById('signin').style.visibility='hidden';
+        document.getElementById('g-signinP').style.height = '0px';
+        document.getElementById('sign-out').style.visibility='visible';
+        document.getElementById('home').style.visibility='visible';
 
-        var prospectFetched = $rootScope.prospectToUpdate;
-
-        console.log("date:", prospectFetched.StartDate);
-        var mydate = prospectFetched.StartDate.toString();
-        var date = mydate.split('T')[0];
-        console.log(date,typeof(date));
-        // var mydate = new Date(prospectFetched.StartDate);
-        // console.log(mydate);
-        $scope.prospect = prospectFetched;
-        $scope.prospect.StartDate = new Date(date);
-        console.log($scope.prospect.StartDate);
+        $scope.maxDate = new Date();
+        $scope.prospect = $rootScope.prospectToUpdate;
 
         $scope.editProspect = function() {
-
             var data = {
                 ProspectID: $rootScope.prospectToUpdate.ProspectID,
                 Name: $scope.prospect.Name,
-                StartDate : $scope.prospect.StartDate,
+                CreateDate : $scope.prospect.CreateDate,
                 TechStack: $scope.prospect.TechStack,
                 Domain: $scope.prospect.Domain,
                 DesiredTeamSize: $scope.prospect.DesiredTeamSize,
@@ -92,62 +149,73 @@ angular.module('PreSales-Huddle')
             console.log(data);
 
             $http.put(baseURL + 'prospect/', data = data).success(function(data, status, headers, config) {
-                console.log('Prospect added.');
+                console.log('Prospect updated.');
+
                 $location.path('/prospects');
             }).error(function(data, status, headers, config) {
                 console.log(data, status, headers, config);
-                console.log('Prospect Not added.');
+                console.log('Prospect not updated.');
             });
         };
 
         // Cancel button function
         $scope.go = function(path) {
-            $rootScope.lastform = "create";
+            $rootScope.lastform = "createProspect";
             $location.path(path);
         }
     })
 
     .controller('AddDiscussionCtrl', function($scope, $http, $rootScope, $location) {
-        $scope.maxDate = new Date();
-        $scope.date = $scope.maxDate;
-        console.log($scope.maxDate);
+        document.getElementById('signin').style.visibility='hidden';
+        document.getElementById('g-signinP').style.height = '0px';
+        document.getElementById('sign-out').style.visibility='visible';
+        document.getElementById('home').style.visibility='visible';
+
+        console.log($rootScope.currentUser);
 
         $scope.addDiscussion = function() {
 
-            // var mydate = $scope.date.toString();
-            // var date = mydate.split('T')[0];
-            // console.log(date,typeof(date));
-            console.log($scope.date);
             var data = {
-                Name: $scope.prospectName,
-                StartDate : $scope.date,
-                TechStack: $scope.techStack,
-                Domain: $scope.domain,
-                DesiredTeamSize: $scope.teamSize,
-                Notes: $scope.notes
+                ProspectID: $rootScope.prospectToUpdate.ProspectID,
+                UserID: $rootScope.currentUser,
+                Query: $scope.query
             };
 
             console.log(data);
 
             $http.post(baseURL + 'discussion/', data = data).success(function(data, status, headers, config) {
                 console.log('Discussion added.');
-                $location.path('/prospects');
+                $location.path('/discussions');
             }).error(function(data, status, headers, config) {
-                console.log('Discussion Not added.');
+                console.log('Discussion not added.');
             });
 
-            $scope.prospectName = "";
-            $scope.date = "";
-            $scope.techStack = "";
-            $scope.domain = "";
-            $scope.teamSize = "";
-            $scope.notes = "";
+            $scope.query = "";
         };
 
         // Cancel button function
         $scope.go = function(path) {
-            $rootScope.lastform = "create";
+            $rootScope.lastform = "createProspect";
             $location.path(path);
         }
     })
+
+    .controller('DiscussionsCtrl', function($scope, $http, $rootScope) {
+
+        document.getElementById('signin').style.visibility='hidden';
+        document.getElementById('g-signinP').style.height = '0px';
+        document.getElementById('sign-out').style.visibility='visible';
+        document.getElementById('home').style.visibility='visible';
+
+        var prospect = $rootScope.prospectToUpdate;
+        console.log(prospect);
+
+        $http.get(baseURL + 'discussion/prospectid/' + prospect.ProspectID).success(function(data, status, headers, config) {
+            console.log(data);
+            $scope.discussions = data;
+            console.log($scope.discussions);
+        }).error(function(data, status, header, config) {});
+    });
+
+
 
